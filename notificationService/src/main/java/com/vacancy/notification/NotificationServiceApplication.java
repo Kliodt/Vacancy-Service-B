37e8e2664c247755d14d14a0515ebc331a70f206
Kliodt <vadimkl2004@mail.ru>
2026-01-10 04:41:55 +0300

    feat: tried kafka

---
+++ notificationService/src/main/java/com/vacancy/notification/NotificationServiceApplication.java
@@ -0,0 +1,18 @@
+package com.vacancy.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@Configuration
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+}
